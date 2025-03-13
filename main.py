0913a4b7ec3771c96de6f2acbdf02d7bf3523f2a
faran <faran.allah.verdi1386@gmail.com>
2025-03-13 13:21:33 +0330

    removed the echo function, built a nice solid foundation

--- main.py
+++ main.py
@@ -10,17 +10,8 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
         chat_id=update.effective_chat.id, 
         text="Register form",
-        message_id=update.effective_message.id
     )
   
-async def echo(update: Update, context : ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(
-        chat_id = update.effective_chat.id,
-        text=update.effective_message.text,
-        message_id = update.effective_message.id,
-    )
-
-
 def main():
     print("bot is running")
 
@@ -28,22 +19,13 @@ def main():
     app = Application.builder().token(API_TOKEN).build()
 
     # command handlers
-    start_handler = CommandHandler("start","s","START", start)
-    
-    # message handlers
-    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    start_handler = CommandHandler("start", start)
 
     # adding the command handelrs to the bot
     app.add_handler(start_handler)
 
-    # adding the message handelrs to the bot
-    app.add_handler(echo_handler)
-
     print("bot is polling")
     app.run_polling()
-    
 
 if __name__ == "__main__":
     main()
-
-
