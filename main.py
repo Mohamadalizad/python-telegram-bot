8fe9b128097de514e3c77594150019bf56ec1d3d
faran <faran.allah.verdi1386@gmail.com>
2025-03-13 13:29:22 +0330

    added a command handler for registration proccess

--- main.py
+++ main.py
@@ -4,25 +4,30 @@ from telegram.ext import Application , CommandHandler, ContextTypes , MessageHan
 
 API_TOKEN = config("API_TOKEN")
 
-# made command handler
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # daryaft chat_id and message_id
     await context.bot.send_message(
         chat_id=update.effective_chat.id, 
         text="Register form",
     )
+
+async def register(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Test"
+    )
+    # username
+    # email
+    # pass
   
 def main():
     print("bot is running")
 
-    # important
     app = Application.builder().token(API_TOKEN).build()
 
-    # command handlers
-    start_handler = CommandHandler("start", start)
-
-    # adding the command handelrs to the bot
-    app.add_handler(start_handler)
+    app.add_handlers([
+        CommandHandler("start", start),
+        CommandHandler("register", register)
+    ])
 
     print("bot is polling")
     app.run_polling()
